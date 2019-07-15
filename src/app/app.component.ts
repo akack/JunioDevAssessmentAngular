6fca390425f6144eea375dc3ae968512fdae7784
akha <akham20@gmail.com>
2019-07-15 15:08:13 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError, map } from 'rxjs/operators';
+import InformationList from './model/InformationList';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+@Injectable()
+export class AppComponent implements OnInit {
+  apiURL = 'http://localhost:50366/api/InformationModel';
   title = 'JuniorDevAssesmmentClient';
+  InformationList: any = [];
+
+  invocation = new XMLHttpRequest();
+
+  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/xml'
+    })
+  };
+
+  ngOnInit(): void {
+    this.loadList();
+  }
+
+  loadList() {
+    return this.getInformation().subscribe((data: {}) => {
+      this.InformationList = data;
+    });
+  }
+  getInformation(): Observable<InformationList> {
+    return this.http.get<InformationList>(this.apiURL)
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  // Error handling
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+ }
 }
+
---
+++ src/app/model/InformationList.js
@@ -0,0 +1,7 @@
+export class InformationList {
+    Certificate_Name: string;
+    Group: string;
+    Issued_date: string;
+    Expiry_date: string;
+    Status: string;
+  }
